6cab7e61e376eced8bb208ea802f117f6b18d1c5
hyyc116 <hyyc116@gmail.com>
2018-03-01 10:58:28 -0500

    add

--- paper_dis_selection.py
+++ paper_dis_selection.py
@@ -10,28 +10,98 @@
 '''
 from basic_config import *
 
-def citation_distribution(papers):
+def gen_paper_attrs(papers,ref_papers):
     '''
         draw paper distribution over year of papers.
         draw citation distribution of papers
 
     '''
-    year_paper_count = defaultdict(int)
-    citation_dis = defaultdict(int)
+    
+    pid_attrs={}
+    progress=1
     for line in open(papers):
+
+        progress+=1
+        if progress%100000==1:
+            logging.info('paper progress {:} ...'.format(progress))
+
+
         line = line.strip()
         pObj = json.loads(line)
-
+        pid = pObj['id']
         year = pObj.get('year',-1)
         if year !=-1:
             year_paper_count[year]+=1
+
+        refs =pObj.get('references',[])
+        refs_dis[len(refs)]+=1
 
         ## if there is no n_citation keywords, return 0
         n_citation = pObj.get('n_citation',0)
         citation_dis[n_citation]+=1
 
+        ## fos
+        fos = pObj.get('fos',[])
+        pid_attrs[pid]['year']=year
+        pid_attrs[pid]['fos']=fos
+        pid_attrs[pid]['refs']=refs
+        pid_attrs[pid]['n_citation']=n_citation
 
-    fig,axes = plt.subplots(2,1,figsize=(10,10))
+    ref_pid_attrs = {}
+    progress=0
+    for line in open(ref_papers):
+        progress+=1
+        if progress%100000==1:
+            logging.info('ref paper progress {:} ...'.format(progress))
+        line = line.strip()
+        pObj = json.loads(line)
+        pid = pObj['id']
+        year = pObj.get('year',-1)
+        refs =pObj.get('references',[])
+        ## if there is no n_citation keywords, return 0
+        n_citation = pObj.get('n_citation',0)
+        ## fos
+        fos = pObj.get('fos',[])
+
+        ref_pid_attrs[pid]['year']=year
+        ref_pid_attrs[pid]['fos']=fos
+        ref_pid_attrs[pid]['refs']=refs
+        ref_pid_attrs[pid]['n_citation']=n_citation
+
+    open('data/paper_attrs.json','w').write(json.dumps(pid_attrs))
+    open('data/ref_paper_attrs.json','w').write(json.dumps(ref_pid_attrs))
+
+def plot_distributions(paper_attrs_path,ref_paper_attrs_path):
+
+    paper_attrs = json.loads(open(paper_attrs_path).read())
+    ref_paper_attrs = json.loads(open(ref_paper_attrs_path).read())
+
+    year_paper_count = defaultdict(int)
+    citation_dis = defaultdict(int)
+    refs_dis = defaultdict(int)
+    citation_age_dis = defaultdict(int)
+
+    for pid in paper_attrs.keys():
+        year = paper_attrs[pid]['year']
+        refs = paper_attrs[pid]['refs']
+        n_citation = paper_attrs[pid]['n_citation']
+        # fos = paper_attrs[pid]['fos']
+        if year!=-1:
+            year_paper_count[year]+=1
+        citation_dis[n_citation]+=1
+        refs_dis[len(refs)]+=1
+
+        ref_years = []
+        for ref in refs:
+            pObj = ref_paper_attrs.get(ref,-1)
+            if pObj!=-1:
+                ref_years.append(pObj.get('year',-1))
+        if year!=-1:
+            citation_age = np.max(ref_years)-year
+            citation_age_dis[citation_age]+=1
+
+
+    fig,axes = plt.subplots(1,4,figsize=(20,5))
     ## plot paper distribution over years
     xs = []
     ys = []
@@ -61,14 +131,33 @@ def citation_distribution(papers):
     ax.set_xscale('log')
     ax.set_yscale('log')
 
+    ## reference num distribution
+    xs = []
+    ys = []
+    for ref in sorted(refs_dis.keys()):
+        xs.append(ref)
+        ys.append(refs_dis[ref])
+
+    ax = axes[2]
+    ax.plot(xs,ys,'o',fillstyle=None)
+    ax.set_xlabel('Citation Count')
+    ax.set_ylabel('Number of Papers')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ## citation age distribution
+
+
+
     plt.tight_layout()
     plt.savefig('figs/paper_distribution.pdf',dpi=200)
 
 
 
 
+
 if __name__ == '__main__':
-    citation_distribution(sys.argv[1])
+    gen_paper_attrs(sys.argv[1],sys.argv[2])
+    # citation_distribution(sys.argv[1])
 
 
 
