e0df4bc59321df5f50b832129f6680fdf7022309
hyyc116 <hyyc116@gmail.com>
2018-03-02 04:04:10 -0500

    add

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 ### select data
-python data_selection.py 
+python data_selection.py select_papers /public/data/Aminer_MAG/MAG/txt/ physics
+
+### output reference papers
+python data_selection.py out_ref_papers /public/data/Aminer_MAG/MAG/txt/ data/physics-ref-ids.txt  physics
+
+### out paper attrs
+python data_selection.py out_paper_attrs data/physics-papers.txt
+
+### out reference attrs
+python out_ref_attrs.py out_paper_attrs data/physics-ref-papers.txt
