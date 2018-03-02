39a3e03b4d0cec4962b3585452f76fd71694fffe
hyyc116 <hyyc116@gmail.com>
2018-03-02 05:17:50 -0500

    add

--- data_selection.py
+++ data_selection.py
@@ -120,11 +120,16 @@ def gen_paper_attrs(papers):
 def gen_ref_paper_attrs(ref_papers):
     ref_pid_attrs=defaultdict(dict)
     progress=0
+    attr_index=1
     for line in open(ref_papers):
-        if progress%100000==0:
+        if progress%10000==0:
             logging.info('ref paper progress {:} ...'.format(progress))
-        
         progress+=1
+
+        if progress%400000==0:
+            open('data/ref_paper_attrs_{:}.json'.format(attr_index),'w').write(json.dumps(ref_pid_attrs))
+            ref_pid_attrs=defaultdict(dict)
+            attr_index+=1
 
         line = line.strip().encode('utf-8',errors='ignore')
         try:
@@ -145,7 +150,7 @@ def gen_ref_paper_attrs(ref_papers):
         ref_pid_attrs[pid]['refs']=refs
         ref_pid_attrs[pid]['n_citation']=n_citation
 
-    open('data/ref_paper_attrs.json','w').write(json.dumps(ref_pid_attrs))
+    open('data/ref_paper_attrs_{:}.json'.format(attr_index),'w').write(json.dumps(ref_pid_attrs))
 
 if __name__ == '__main__':
 
