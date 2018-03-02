ccbb9fd05e2a22b69932aa34dc5a68fc26d61bf7
hyyc116 <hyyc116@gmail.com>
2018-03-02 16:39:05 -0500

    add

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ python data_selection.py select_papers /public/data/Aminer_MAG/MAG/txt/ physics
 python data_selection.py out_ref_papers /public/data/Aminer_MAG/MAG/txt/ data/physics-ref-ids.txt  physics
 
 ### citing relation
-python data_selection.py out_ref_papers /public/data/Aminer_MAG/MAG/txt/ data/physics-paper-ids.txt
+python data_selection.py citing_relation /public/data/Aminer_MAG/MAG/txt/ data/physics-paper-ids.txt
 
 ### out paper attrs
 python data_selection.py out_paper_attrs data/physics-papers.txt
