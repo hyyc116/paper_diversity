75ecc5e9b1f6659bb094a7ddc132345b200ccc83
hyyc116 <hyyc116@gmail.com>
2018-03-27 10:45:18 -0400

    add

--- wos_data_visualizing.py
+++ wos_data_visualizing.py
@@ -185,7 +185,7 @@ def plot_statistics(cc_count_path,year_numbers_path,year_cc_path,ref_num_count_p
 
     l4 = ax3.plot(xs,[cc_mean]*len(xs),'--',c=color_sequence[2],label='average citation count:{:.2f}'.format(cc_mean))
 
-    ax3.text(2003,np.mean(year_cc[2003]),'(2003,{:.2f})'.format(np.mean(year_cc[2003])))
+    ax3.text(2003,np.mean(year_cc['2003']),'(2003,{:.2f})'.format(np.mean(year_cc['2003'])))
     
     ax3.set_ylabel('average citation count')
     ax3.set_yscale('log')
