0e1ca6b4dd0c437e49d9dcaa6349d77eca93a897
hyyc116 <hyyc116@gmail.com>
2018-04-22 20:19:13 -0400

    add

--- plot_figs_from_data_folder.py
+++ plot_figs_from_data_folder.py
@@ -201,7 +201,7 @@ def plotting_from_data_folder():
     plt.savefig('pdf/figs/cc_yd.jpg',dpi=400)
 
 
-    dvs_imp_fig_data['ys_cc'] = [xs,ys]
+    xs,ys = dvs_imp_fig_data['yd_cc']
     plt.figure()
     plt.plot(xs,ys,c=color_sequence[0])
     plt.ylabel('average citation count')
