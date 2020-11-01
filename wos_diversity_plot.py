79cbda3835fe0e447f5aaacc411110401a52c79b
hyyc116 <hyyc116@gmail.com>
2020-11-01 18:06:30 +0800

    Update wos_diversity_plot.py

--- wos_diversity_plot.py
+++ wos_diversity_plot.py
@@ -247,7 +247,8 @@ def attr1_over_attr2(attr1,attr2,all_subjs,attr_name_1,attr_name_2,save_name,axr
         #  一种是用moving average
         window = 5
         if xscale=='log':  
-            window=55
+            window=9
+            
         ax.plot(xs,smooth(ys_mean,window),'--',label='mean')
         ax.plot(xs,smooth(ys_median,window),'-.',label='median')
         #  先不进行平滑
