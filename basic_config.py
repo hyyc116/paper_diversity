e6de207e1ec652291e55b848775ada5f86e67fbb
hyyc116 <hyyc116@gmail.com>
2021-04-07 21:17:13 +0800

    Update basic_config.py

--- basic_config.py
+++ basic_config.py
@@ -40,17 +40,19 @@ import scipy
 
 from skmisc.loess import loess
 
-
 # from gini import gini
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=logging.INFO)
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    level=logging.INFO)
 
 mpl.rcParams['agg.path.chunksize'] = 10000
 
-color_sequence = ['#1f77b4',   '#ffbb78', '#2ca02c',
-                   '#d62728', '#ff9896', '#9467bd',
-                  '#8c564b', '#e377c2', '#7f7f7f',
-                   '#bcbd22', '#dbdb8d', '#17becf','#ff7f0e', '#aec7e8','#9edae5','#98df8a', '#c5b0d5', '#c49c94', '#f7b6d2','#c7c7c7']
+color_sequence = [
+    '#1f77b4', '#ffbb78', '#2ca02c', '#d62728', '#ff9896', '#9467bd',
+    '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#dbdb8d', '#17becf',
+    '#ff7f0e', '#aec7e8', '#9edae5', '#98df8a', '#c5b0d5', '#c49c94',
+    '#f7b6d2', '#c7c7c7'
+]
 mpl.rcParams['axes.prop_cycle'] = cycler('color', color_sequence)
 
 # color = plt.cm.viridis(np.linspace(0.01,0.99,6)) # This returns RGBA; convert:
@@ -59,103 +61,123 @@ mpl.rcParams['axes.prop_cycle'] = cycler('color', color_sequence)
 
 # mpl.rcParams['axes.prop_cycle'] = cycler('color', hexcolor)
 
-
-params = {'legend.fontsize': 10,
-         'axes.labelsize': 15,
-         'axes.titlesize':20,
-         'xtick.labelsize':15,
-         'ytick.labelsize':15}
+params = {
+    'legend.fontsize': 10,
+    'axes.labelsize': 15,
+    'axes.titlesize': 20,
+    'xtick.labelsize': 15,
+    'ytick.labelsize': 15
+}
 pylab.rcParams.update(params)
-
 
 # from paths import *
 
 
-def circle(ax,x,y,radius=0.15):
+def circle(ax, x, y, radius=0.15):
 
-    circle = Circle((x, y), radius, clip_on=False, zorder=10, linewidth=1,
-                    edgecolor='black', facecolor=(0, 0, 0, .0125),
+    circle = Circle((x, y),
+                    radius,
+                    clip_on=False,
+                    zorder=10,
+                    linewidth=1,
+                    edgecolor='black',
+                    facecolor=(0, 0, 0, .0125),
                     path_effects=[withStroke(linewidth=5, foreground='w')])
     ax.add_artist(circle)
 
-def power_low_func(x,a,b):
-    return b*(x**(-a))
 
-def exponential_func(x,a,b):
-    return b*np.exp(-a*x)
+def power_low_func(x, a, b):
+    return b * (x**(-a))
 
-def square_x(x,a,b,c):
-  return a*pow(x,2)+b*x+c
 
-def autolabel(rects,ax,total_count=None,step=1,):
+def exponential_func(x, a, b):
+    return b * np.exp(-a * x)
+
+
+def square_x(x, a, b, c):
+    return a * pow(x, 2) + b * x + c
+
+
+def autolabel(
+    rects,
+    ax,
+    total_count=None,
+    step=1,
+):
     """
     Attach a text label above each bar displaying its height
     """
-    for index in np.arange(len(rects),step=step):
+    for index in np.arange(len(rects), step=step):
         rect = rects[index]
         height = rect.get_height()
         # print height
         if not total_count is None:
-            ax.text(rect.get_x() + rect.get_width()/2., 1.005*height,
-                    '{:}\n({:.6f})'.format(int(height),height/float(total_count)),
-                    ha='center', va='bottom')
+            ax.text(rect.get_x() + rect.get_width() / 2.,
+                    1.005 * height,
+                    '{:}\n({:.6f})'.format(int(height),
+                                           height / float(total_count)),
+                    ha='center',
+                    va='bottom')
         else:
-            ax.text(rect.get_x() + rect.get_width()/2., 1.005*height,
+            ax.text(rect.get_x() + rect.get_width() / 2.,
+                    1.005 * height,
                     '{:}'.format(int(height)),
-                    ha='center', va='bottom')
+                    ha='center',
+                    va='bottom')
 
 
 class dbop:
+    def __init__(self, insert_index=0):
 
-    def __init__(self,insert_index=0):
-
-        self._insert_index=insert_index
-        self._insert_values=[]
+        self._insert_index = insert_index
+        self._insert_values = []
         logging.debug("connect database with normal cursor.")
-        self._db = psycopg2.connect(database='core_data',user="buyi",password = "ruth_hardtop_isthmus_bubbly")
+        self._db = psycopg2.connect(database='core_data',
+                                    user="buyi",
+                                    password="ruth_hardtop_isthmus_bubbly")
         self._cursor = self._db.cursor()
 
-
-
-    def query_database(self,sql):
+    def query_database(self, sql):
         self._cursor.close()
         self._cursor = self._db.cursor()
         self._cursor.execute(sql)
         logging.debug("query database with sql {:}".format(sql))
         return self._cursor
 
-    def insert_database(self,sql,values):
+    def insert_database(self, sql, values):
         self._cursor.close()
         self._cursor = self._db.cursor()
-        self._cursor.executemany(sql,values)
+        self._cursor.executemany(sql, values)
         logging.debug("insert data to database with sql {:}".format(sql))
         self._db.commit()
 
-
-    def batch_insert(self,sql,row,step,is_auto=True,end=False):
+    def batch_insert(self, sql, row, step, is_auto=True, end=False):
         if end:
-            if len(self._insert_values)!=0:
-                logging.info("insert {:}th data into database,final insert.".format(self._insert_index))
-                self.insert_database(sql,self._insert_values)
+            if len(self._insert_values) != 0:
+                logging.info(
+                    "insert {:}th data into database,final insert.".format(
+                        self._insert_index))
+                self.insert_database(sql, self._insert_values)
         else:
-            self._insert_index+=1
+            self._insert_index += 1
             if is_auto:
                 row[0] = self._insert_index
             self._insert_values.append(tuple(row))
-            if self._insert_index%step==0:
-                logging.info("insert {:}th data into database".format(self._insert_index))
-                self.insert_database(sql,self._insert_values)
-                self._insert_values=[]
+            if self._insert_index % step == 0:
+                logging.info("insert {:}th data into database".format(
+                    self._insert_index))
+                self.insert_database(sql, self._insert_values)
+                self._insert_values = []
 
     def get_insert_count(self):
         return self._insert_index
 
-    def execute_del_update(self,sql):
+    def execute_del_update(self, sql):
         self._cursor.execute(sql)
         self._db.commit()
         logging.debug("execute delete or update sql {:}.".format(sql))
 
-    def execute_sql(self,sql):
+    def execute_sql(self, sql):
         self._cursor.execute(sql)
         self._db.commit()
         logging.debug("execute sql {:}.".format(sql))
@@ -164,8 +186,7 @@ class dbop:
         self._db.close()
 
 
-
-def plot_line_from_data(fig_data,ax=None):
+def plot_line_from_data(fig_data, ax=None):
 
     xs = fig_data['x']
     ys = fig_data['y']
@@ -173,13 +194,12 @@ def plot_line_from_data(fig_data,ax=None):
     xlabel = fig_data['xlabel']
     ylabel = fig_data['ylabel']
     marker = fig_data['marker']
-    xscale = fig_data.get('xscale','linear')
-    yscale = fig_data.get('yscale','linear')
-
+    xscale = fig_data.get('xscale', 'linear')
+    yscale = fig_data.get('yscale', 'linear')
 
     if ax is None:
 
-        plt.plot(xs,ys,marker)
+        plt.plot(xs, ys, marker)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
         plt.xscale(xscale)
@@ -189,27 +209,27 @@ def plot_line_from_data(fig_data,ax=None):
 
     else:
 
-        ax.plot(xs,ys,marker)
+        ax.plot(xs, ys, marker)
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
         ax.set_title(title)
         ax.set_xscale(xscale)
         ax.set_yscale(yscale)
 
-def plot_bar_from_data(fig_data,ax=None):
+
+def plot_bar_from_data(fig_data, ax=None):
 
     xs = fig_data['x']
     ys = fig_data['y']
     title = fig_data['title']
     xlabel = fig_data['xlabel']
     ylabel = fig_data['ylabel']
-    xscale = fig_data.get('xscale','linear')
-    yscale = fig_data.get('yscale','linear')
-
+    xscale = fig_data.get('xscale', 'linear')
+    yscale = fig_data.get('yscale', 'linear')
 
     if ax is None:
 
-        plt.bar(xs,ys,align='center')
+        plt.bar(xs, ys, align='center')
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
         plt.xscale(xscale)
@@ -219,7 +239,7 @@ def plot_bar_from_data(fig_data,ax=None):
 
     else:
 
-        ax.bar(xs,ys,align='center')
+        ax.bar(xs, ys, align='center')
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
         ax.set_title(title)
@@ -227,8 +247,7 @@ def plot_bar_from_data(fig_data,ax=None):
         ax.set_yscale(yscale)
 
 
-
-def plot_multi_lines_from_data(fig_data,ax=None):
+def plot_multi_lines_from_data(fig_data, ax=None):
 
     xs = fig_data['x']
     yses = fig_data['ys']
@@ -237,13 +256,12 @@ def plot_multi_lines_from_data(fig_data,ax=None):
     ylabel = fig_data['ylabel']
     markers = fig_data['markers']
     labels = fig_data['labels']
-    xscale = fig_data.get('xscale','linear')
-    yscale = fig_data.get('yscale','linear')
-
+    xscale = fig_data.get('xscale', 'linear')
+    yscale = fig_data.get('yscale', 'linear')
 
     if ax is None:
-        for i,ys in enumerate(yses):
-            plt.plot(xs,ys,markers[i],label=labels[i])
+        for i, ys in enumerate(yses):
+            plt.plot(xs, ys, markers[i], label=labels[i])
 
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
@@ -254,8 +272,8 @@ def plot_multi_lines_from_data(fig_data,ax=None):
         plt.tight_layout()
 
     else:
-        for i,ys in enumerate(yses):
-            ax.plot(xs,ys,markers[i],label=labels[i])
+        for i, ys in enumerate(yses):
+            ax.plot(xs, ys, markers[i], label=labels[i])
 
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
@@ -265,7 +283,7 @@ def plot_multi_lines_from_data(fig_data,ax=None):
         ax.legend()
 
 
-def plot_multi_lines_from_two_data(fig_data,ax=None):
+def plot_multi_lines_from_two_data(fig_data, ax=None):
 
     xses = fig_data['xs']
     yses = fig_data['ys']
@@ -274,12 +292,12 @@ def plot_multi_lines_from_two_data(fig_data,ax=None):
     ylabel = fig_data['ylabel']
     markers = fig_data['markers']
     labels = fig_data['labels']
-    xscale = fig_data.get('xscale','linear')
-    yscale = fig_data.get('yscale','linear')
+    xscale = fig_data.get('xscale', 'linear')
+    yscale = fig_data.get('yscale', 'linear')
 
     if ax is None:
-        for i,ys in enumerate(yses):
-            plt.plot(xses[i],ys,markers[i],label=labels[i])
+        for i, ys in enumerate(yses):
+            plt.plot(xses[i], ys, markers[i], label=labels[i])
 
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
@@ -290,8 +308,8 @@ def plot_multi_lines_from_two_data(fig_data,ax=None):
         plt.tight_layout()
 
     else:
-        for i,ys in enumerate(yses):
-            ax.plot(xses[i],ys,markers[i],label=labels[i])
+        for i, ys in enumerate(yses):
+            ax.plot(xses[i], ys, markers[i], label=labels[i])
 
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
@@ -301,9 +319,10 @@ def plot_multi_lines_from_two_data(fig_data,ax=None):
         ax.legend()
 
 
-def hist_2_bar(data,bins=50):
-    n,bins,patches = plt.hist(data,bins=bins)
-    return [x for x in bins[:-1]],[x for x in n]
+def hist_2_bar(data, bins=50):
+    n, bins, patches = plt.hist(data, bins=bins)
+    return [x for x in bins[:-1]], [x for x in n]
+
 
 def gini(array):
     """Calculate the Gini coefficient of a numpy array."""
@@ -312,7 +331,7 @@ def gini(array):
     # from:
     # http://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
     # All values are treated equally, arrays must be 1d:
-    array = array.flatten()
+    array = np.array(array).flatten()
     if np.amin(array) < 0:
         # Values cannot be negative:
         array -= np.amin(array)
@@ -321,51 +340,51 @@ def gini(array):
     # Values must be sorted:
     array = np.sort(array)
     # Index per array element:
-    index = np.arange(1,array.shape[0]+1)
+    index = np.arange(1, array.shape[0] + 1)
     # Number of array elements:
     n = array.shape[0]
     # Gini coefficient:
-    return ((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array)))
+    return ((np.sum((2 * index - n - 1) * array)) / (n * np.sum(array)))
+
 
 def loess_test():
 
-    x = np.linspace(0,2*np.pi,100).tolist()*100
+    x = np.linspace(0, 2 * np.pi, 100).tolist() * 100
 
     x = sorted(x)
-    y = np.sin(x) + np.random.random(10000) 
+    y = np.sin(x) + np.random.random(10000)
 
-    pred_x,lowess,ll,ul = loess_data(x,y)
+    pred_x, lowess, ll, ul = loess_data(x, y)
 
     plt.figure()
     # plt.plot(x, y, '+')
     plt.plot(pred_x, lowess)
-    plt.fill_between(pred_x,ll,ul,alpha=.5,color='#ff9896')
+    plt.fill_between(pred_x, ll, ul, alpha=.5, color='#ff9896')
     plt.savefig('test_loess.png')
 
-    xs,ys_mean,ll,ul = average_with_95_confidence(x,y)
+    xs, ys_mean, ll, ul = average_with_95_confidence(x, y)
 
     plt.figure()
     # plt.plot(x, y, '+')
     plt.plot(xs, ys_mean)
-    plt.fill_between(xs,ll,ul,alpha=.5,color='#ff9896')
+    plt.fill_between(xs, ll, ul, alpha=.5, color='#ff9896')
     plt.savefig('test_average_confidence.png')
 
 
-
 ## use local weighted regression to fit data
-def loess_data(xs,ys):
+def loess_data(xs, ys):
 
     ixes = range(len(xs))
 
     sorted_xs = []
     sorted_ys = []
 
-    for ix in sorted(ixes,key=lambda x:xs[x]):
+    for ix in sorted(ixes, key=lambda x: xs[x]):
 
         sorted_xs.append(xs[ix])
         sorted_ys.append(ys[ix])
 
-    l = loess(sorted_xs,sorted_ys)
+    l = loess(sorted_xs, sorted_ys)
     l.fit()
 
     pred_x = sorted(list(set(sorted_xs)))
@@ -376,17 +395,17 @@ def loess_data(xs,ys):
     ll = conf.lower
     ul = conf.upper
 
-    return pred_x,lowess,ll,ul
+    return pred_x, lowess, ll, ul
+
 
 ## use bootstrap method to cal mean and confidence interval
 
 
-def average_with_95_confidence(xs,ys):
+def average_with_95_confidence(xs, ys):
 
     x_y_dict = defaultdict(list)
     for ix in range(len(xs)):
         x_y_dict[xs[ix]].append(ys[ix])
-
 
     xs = []
     ys_mean = []
@@ -400,15 +419,12 @@ def average_with_95_confidence(xs,ys):
 
         ys_mean.append(mean_func(data))
 
-        ll,ul = bootstrap(data,1000,0.95,mean_func)
+        ll, ul = bootstrap(data, 1000, 0.95, mean_func)
 
         ys_ul.append(ul)
         ys_ll.append(ll)
 
-    return xs,ys_mean,ys_ll,ys_ul
-
-
-
+    return xs, ys_mean, ys_ll, ys_ul
 
 
 def bootstrap(data, B, c, func):
@@ -458,7 +474,3 @@ if __name__ == '__main__':
     # plt.savefig('fig/test_color.jpg')
 
     loess_test()
-
-
-
-
