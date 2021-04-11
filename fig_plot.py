5c89302bb2fddefa73f283f4d9d6549ab8cad84b
hyyc116 <hyyc116@gmail.com>
2021-04-11 11:04:46 +0800

    Update fig_plot.py

--- fig_plot.py
+++ fig_plot.py
@@ -1,5 +1,7 @@
 #coding:utf-8
 from basic_config import *
+import pandas as pd
+import seaborn as sns
 
 
 def plot_fig2():
@@ -62,5 +64,105 @@ def plot_fig2():
     logging.info('fig saved to fig/fig2.png.')
 
 
+# A Freshness diversity的CDF分布
+# B FD 随着时间的变化
+# C FD 与C5的相关关系
+# D FD与C10的关系
+def plot_fig3():
+
+    all_data = pd.read_csv('data/ALL_attrs.txt')
+    shuffed_data = pd.read_csv('data/new_shuffled_yd_lines.csv')
+
+    _, axes = plt.subplots(2, 2, figsize=(10, 8))
+
+    ax = axes[0][0]
+    # CDF分布
+    sns.histplot(all_data,
+                 x='yd_div',
+                 ax=ax,
+                 kde=True,
+                 cumulative=True,
+                 hue='subjs',
+                 fill=False,
+                 stat='probability')
+
+    # 所有的分布
+    sns.histplot(all_data,
+                 x='yd_div',
+                 ax=ax,
+                 kde=True,
+                 cumulative=True,
+                 fill=False,
+                 stat='probability',
+                 label='ALL')
+
+    sns.histplot(shuffed_data,
+                 x='yd_div',
+                 ax=ax,
+                 kde=True,
+                 cumulative=True,
+                 fill=False,
+                 stat='probability',
+                 label='NULLMODEL')
+
+    ax.set_xlabel('freshness diversity')
+
+    ax.legend()
+
+    axb = axes[0][1]
+    sns.lineplot(all_data, x='year', y='yd_div', ax=axb, ci=95, hue='subjs')
+    sns.lineplot(all_data, x='year', y='yd_div', ax=axb, ci=95, label='ALL')
+    sns.lineplot(shuffed_data,
+                 x='year',
+                 y='yd_div',
+                 ax=axb,
+                 ci=95,
+                 label='NULLMODEL')
+
+    axb.set_xlabel('year')
+    axb.set_ylabel('freshness diversity')
+
+    axb.legend()
+
+    axc = axes[1][0]
+
+    sns.lineplot(all_data, x='c10', y='yd_div', ax=axc, ci=95, hue='subjs')
+    sns.lineplot(all_data, x='c10', y='yd_div', ax=axc, ci=95, label='ALL')
+    sns.lineplot(shuffed_data,
+                 x='c10',
+                 y='yd_div',
+                 ax=axc,
+                 ci=95,
+                 label='NULLMODEL')
+
+    axc.set_xlabel('$c_{10}$')
+    axc.set_ylabel('freshness diversity')
+
+    axc.set_xscale('log')
+
+    axc = axes[1][1]
+
+    sns.lineplot(all_data, x='c5', y='yd_div', ax=axc, ci=95, hue='subjs')
+    sns.lineplot(all_data, x='c5', y='yd_div', ax=axc, ci=95, label='ALL')
+    sns.lineplot(shuffed_data,
+                 x='c5',
+                 y='yd_div',
+                 ax=axc,
+                 ci=95,
+                 label='NULLMODEL')
+
+    axc.set_xlabel('$c_5$')
+    axc.set_ylabel('freshness diversity')
+
+    axc.set_xscale('log')
+
+    plt.tight_layout()
+
+    plt.savefig('fig/fig3.png', dpi=400)
+    logging.info('fig saved to fig/fig3.png.')
+
+
 if __name__ == '__main__':
-    plot_fig2()
+    # plot_fig2()
+
+    plot_fig3()
