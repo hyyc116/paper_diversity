3c19d885416d78edeec243da70bfd26a57ba7adf
hyyc116 <hyyc116@gmail.com>
2022-01-12 10:23:24 +0800

    Update abs_data.py

--- src/data_process/abs_data.py
+++ src/data_process/abs_data.py
@@ -3,6 +3,13 @@ import sys
 sys.path.append('src')
 from basic_config import *
 
+'''
+一共117本ABS四星以上期刊，一共找到91本，放在了data/abs.used_journal.txt
+
+
+
+'''
+
 def filter_4_star_journal():
     journal_names = ['jname==star']
     for line in open('data/abs.journal.txt'):
@@ -41,8 +48,27 @@ def search_abs_journal_paper():
 
     logging.info(f'{len(jnames)} journals in total, found {len(jids)} journals in MAG.')
 
+def get_abs_paper_ids():
+    jids  = set([line.strip() for line in open('data/abs_jid.txt')])
+
+    query_op = dbop()
+    sql = 'select paper_id,journal_id from mag_core.papers'
+    pid_jid = {}
+    pids = []
+
+    for pid,jid in query_op.query_database(sql):
+        if str(jid) in jids:
+            pid_jid[pid] = jid
+            pids.append(pid)
+
+    open('data/abs.paper_jid.json','w').write(json.dumps(pid_jid))
+
+    open('data/abs_pids.txt','w').write('\n'.join(pids))
+
+    logging.info(f'{len(pids)} abs papers founded.')
 
 
 if __name__ == "__main__":
     # filter_4_star_journal()
-    search_abs_journal_paper()
+    # search_abs_journal_paper()
+    get_abs_paper_ids()
