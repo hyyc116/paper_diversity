263ea4c2551c4067b4274e5a43b274311e8155d6
hyyc116 <hyyc116@gmail.com>
2022-02-19 17:15:36 +0800

    Update abs_regression.py

--- src/data_process/abs_regression.py
+++ src/data_process/abs_regression.py
@@ -37,6 +37,10 @@ def regress_FE():
     fe_res = mod.fit()
     print(fe_res)
 
+    mod = PanelOLS(data10.c10, exog, entity_effects=True, time_effects=True)
+    fe_res = mod.fit()
+    print(fe_res)
+
 
 if __name__ =="__main__":
 
